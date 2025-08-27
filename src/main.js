9d200db0a951ac074d01e3e37b35f77535f67a67
santiagoCQ04 <santiagocarvajalquiroga@gmail.com>
2025-08-27 17:26:24 -0500

    subiendo cambios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import { router } from './routes/routes'
 import './style.css'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+    plugins: {},
+})
+
+myApp.use(router)
+
+myApp.mount('#app')
+
+// createApp(App).mount('#app')
---
+++ src/routes/routes.js
@@ -0,0 +1,15 @@
+
+import { createRouter, createWebHashHistory } from "vue-router"
+
+/* const routes = [
+    {path: "/", component:presentation},
+    {path: "/extra_meals", component:extra_meals},
+    {path: "/drinks", component:drinks},
+    {path: "/dogs_and_sausages_and_fries", component:dogs_and_sausages_and_fries},
+    {path: "/burgers", component:burgers}
+
+] */
+export const router = createRouter({
+    history: createWebHashHistory(),
+ 
+})
