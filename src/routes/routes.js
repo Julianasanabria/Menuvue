33133316f0c480bd18365c911815133bfb542ecf
santiagoCQ04 <santiagocarvajalquiroga@gmail.com>
2025-08-27 17:38:13 -0500

    segundo cambios

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,10 @@
 
 import { createRouter, createWebHashHistory } from "vue-router"
 
-/* const routes = [
-    {path: "/", component:presentation},
-    {path: "/extra_meals", component:extra_meals},
-    {path: "/drinks", component:drinks},
-    {path: "/dogs_and_sausages_and_fries", component:dogs_and_sausages_and_fries},
-    {path: "/burgers", component:burgers}
-
-] */
+const routes = [
+   
+] 
 export const router = createRouter({
     history: createWebHashHistory(),
- 
+    routes
 })
